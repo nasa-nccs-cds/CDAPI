0501c8971c669e680bba410b214959de9e4f3002
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-01-22 16:31:58 -0500

    Initial adjustments

--- src/main/scala/nccs/engine/manager.scala
+++ src/main/scala/nccs/engine/manager.scala
@@ -2,12 +2,17 @@ package nccs.engine
 import nccs.process.TaskRequest
 import org.slf4j.LoggerFactory
 
-class ExecutionManager {}
-
-object ExecutionManager {
+abstract class ExecutionManager {
   val logger = LoggerFactory.getLogger( classOf[ExecutionManager] )
 
-  def execute( request: TaskRequest, run_args: Map[String,Any] ) = {
+  def execute( process_name: String, datainputs: Map[String, Seq[Map[String, Any]]], run_args: Map[String,Any] ): xml.Elem
+}
+
+object demoExecutionManager extends ExecutionManager {
+
+  override def execute( process_name: String, datainputs: Map[String, Seq[Map[String, Any]]], run_args: Map[String,Any] ): xml.Elem = {
+    val request = TaskRequest( process_name, datainputs )
     logger.info("Execute { request: " + request.toString + ", runargs: " + run_args.toString + "}"  )
+    request.toXml()
   }
 }
