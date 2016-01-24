9cee0b5eb6f330d5f0ef42907989cc959ab51bcf
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-01-24 17:24:34 -0500

    Improve GenericNumber

--- src/main/scala/nccs/esgf/utilities/numbers.scala
+++ src/main/scala/nccs/esgf/utilities/numbers.scala
@@ -7,6 +7,8 @@ object GenericNumber {
   implicit def float2gen( numvalue: Float ): GenericNumber = { new FloatNumber(numvalue) }
   implicit def dbl2gen( numvalue: Double ): GenericNumber = { new DoubleNumber(numvalue) }
   implicit def short2gen( numvalue: Short ): GenericNumber = { new ShortNumber(numvalue) }
+  implicit def gen2int( gennum: GenericNumber ): Int = { gennum.toInt }
+  implicit def gen2float( gennum: GenericNumber ): Float = { gennum.toFloat }
 
   def fromString(sx: String): GenericNumber = {
     try {
@@ -36,31 +38,43 @@ abstract class GenericNumber {
   type NumericType
   def value: NumericType
   override def toString = value.toString
+  def toInt: Int
+  def toFloat: Float
 }
 
 class IntNumber( val numvalue: Int ) extends GenericNumber {
   type NumericType = Int
   override def value: NumericType = numvalue
+  override def toInt: Int = value
+  override def toFloat: Float = value.toFloat
 }
 
 class FloatNumber( val numvalue: Float ) extends GenericNumber {
   type NumericType = Float
   override def value: NumericType =  numvalue
+  override def toInt: Int = value.toInt
+  override def toFloat: Float = value
 }
 
 class DoubleNumber( val numvalue: Double ) extends GenericNumber {
   type NumericType = Double
   override def value: NumericType = numvalue
+  override def toInt: Int = value.toInt
+  override def toFloat: Float = value.toFloat
 }
 
 class ShortNumber( val numvalue: Short ) extends GenericNumber {
   type NumericType = Short
   override def value: NumericType = numvalue
+  override def toInt: Int = value.toInt
+  override def toFloat: Float = value.toFloat
 }
 
 class UndefinedNumber extends GenericNumber {
   type NumericType = Option[Any]
   override def value: NumericType = None
+  override def toInt: Int = throw new Exception( "Attempt to access an UndefinedNumber ")
+  override def toFloat: Float = throw new Exception( "Attempt to access an UndefinedNumber ")
 }
 
 object testNumbers extends App {
