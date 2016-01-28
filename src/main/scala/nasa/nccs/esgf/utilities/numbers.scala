6e02793dea883bb6c48c5f78e6ee4636469bcb2e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-01-28 17:32:37 -0500

    improve GenericNumber

--- src/main/scala/nasa/nccs/esgf/utilities/numbers.scala
+++ src/main/scala/nasa/nccs/esgf/utilities/numbers.scala
@@ -1,5 +1,7 @@
 package nasa.nccs.esgf.utilities.numbers
 
+import org.slf4j.LoggerFactory
+
 class IllegalNumberException( value: Any ) extends RuntimeException("Error, " + value.toString + " is not a valid Number")
 
 object GenericNumber {
@@ -9,6 +11,7 @@ object GenericNumber {
   implicit def short2gen( numvalue: Short ): GenericNumber = { new ShortNumber(numvalue) }
   implicit def gen2int( gennum: GenericNumber ): Int = { gennum.toInt }
   implicit def gen2float( gennum: GenericNumber ): Float = { gennum.toFloat }
+  implicit def gen2double( gennum: GenericNumber ): Double = { gennum.toDouble }
 
   def fromString(sx: String): GenericNumber = {
     try {
@@ -35,39 +38,45 @@ object GenericNumber {
 }
 
 abstract class GenericNumber {
+  val logger = LoggerFactory.getLogger( classOf[GenericNumber] )
   type NumericType
   def value: NumericType
   override def toString = value.toString
   def toInt: Int
   def toFloat: Float
+  def toDouble: Double
 }
 
 class IntNumber( val numvalue: Int ) extends GenericNumber {
   type NumericType = Int
   override def value: NumericType = numvalue
   override def toInt: Int = value
-  override def toFloat: Float = value.toFloat
+  override def toFloat: Float = { logger.warn( s"Converting Int $value to Float"); value.toFloat }
+  override def toDouble: Double = { logger.warn( s"Converting Int $value to Double"); value.toDouble }
 }
 
 class FloatNumber( val numvalue: Float ) extends GenericNumber {
   type NumericType = Float
   override def value: NumericType =  numvalue
-  override def toInt: Int = value.toInt
+  override def toInt: Int = { logger.warn( s"Converting Float $value to Int"); value.toInt }
   override def toFloat: Float = value
+  override def toDouble: Double = value.toDouble
 }
 
 class DoubleNumber( val numvalue: Double ) extends GenericNumber {
   type NumericType = Double
   override def value: NumericType = numvalue
-  override def toInt: Int = value.toInt
+  override def toInt: Int = { logger.warn( s"Converting Double $value to Int"); value.toInt }
   override def toFloat: Float = value.toFloat
+  override def toDouble: Double = value
 }
 
 class ShortNumber( val numvalue: Short ) extends GenericNumber {
   type NumericType = Short
   override def value: NumericType = numvalue
   override def toInt: Int = value.toInt
-  override def toFloat: Float = value.toFloat
+  override def toFloat: Float = { logger.warn(s"Converting Short $value to Float"); value.toFloat }
+  override def toDouble: Double = { logger.warn(s"Converting Short $value to Double"); value.toDouble }
 }
 
 class UndefinedNumber extends GenericNumber {
@@ -75,6 +84,7 @@ class UndefinedNumber extends GenericNumber {
   override def value: NumericType = None
   override def toInt: Int = throw new Exception( "Attempt to access an UndefinedNumber ")
   override def toFloat: Float = throw new Exception( "Attempt to access an UndefinedNumber ")
+  override def toDouble: Double = throw new Exception( "Attempt to access an UndefinedNumber ")
 }
 
 object testNumbers extends App {
